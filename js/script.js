972002fd7db85e9baecac2e7b6954643d3f05b06
AlessioDPT <alessio.recos@gmail.com>
2021-05-26 17:16:12 +0200

    ADD-var nome/colore/psw/bonus

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 // prova config script
-var prova = "prova testo"
+var prova = "prova generale"
 console.log(prova)
 
+
+
+// prompt nome
+var nomeUtente = prompt ("Inserisci il tuo NOME qui!").toLowerCase();
+console.log("nome :", nomeUtente);
+
+
+// prompt cognome
+var cognomeUtente = prompt ("Inserisci il tuo cognome qui!").toLowerCase();
+console.log("cognome :", cognomeUtente);
+
+// prompt colore
+var coloreUtente = prompt ("Inserisci il tuo colore qui!").toLowerCase();
+console.log("colore preferito :", coloreUtente);
+
+
+
+// addizione risultati
+var psw = nomeUtente + cognomeUtente + coloreUtente + "21";
+console.log("password segretissima :", psw).toLowerCase();
+document.write(psw);
